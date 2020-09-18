d530844afd958ba83a3ea0b527c1dce99fb9a634
Cristian Racedo <CristianDRacedo@hotmail.com>
2020-09-17 22:10:13 -0300

    Menu Responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 /*Responsive Web Design*/
-
+	//Menu Desplegable
+	let openMenu = true;
+	document.querySelector('#menu').addEventListener(
+		'click', () => {
+			document.querySelector('.menu').classList.toggle('openMenu');
+			if (openMenu) {
+				document.querySelector('#menu').innerHTML = '&times;'
+				openMenu = false;
+			} else {
+				document.querySelector('#menu').innerHTML = '&equiv;'
+				openMenu = true;
+			}
+		}
+	);
 /*Autor: Cristian Racedo*/
