38eae735e156d32769dc49d0364f07ca7a53e596
C215714N <CristianDRacedo@hotmail.com>
2020-10-08 20:47:24 -0300

    Galeria Bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 /*Responsive Web Design*/
 	//Menu Desplegable
-	let openMenu = true;
-	document.querySelector('#menu').addEventListener(
-		'click', () => {
-			document.querySelector('.menu').classList.toggle('openMenu');
-			if (openMenu) {
-				document.querySelector('#menu').innerHTML = '&times;'
-				openMenu = false;
-			} else {
-				document.querySelector('#menu').innerHTML = '&equiv;'
-				openMenu = true;
+		let openMenu = true;
+		document.querySelector('#menu').addEventListener(
+			'click', () => {
+				document.querySelector('.menu').classList.toggle('openMenu');
+				if (openMenu) {
+					document.querySelector('#menu').innerHTML = '&times;'
+					openMenu = false;
+				} else {
+					document.querySelector('#menu').innerHTML = '&equiv;'
+					openMenu = true;
+				}
 			}
-		}
-	);
+		);
+		
 /*Autor: Cristian Racedo*/
