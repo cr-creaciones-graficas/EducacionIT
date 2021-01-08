8f752a2a80609ffb7f3667430bce0021b09a1547
c215714n <cristiandracedo@hotmail.com>
2021-01-08 02:56:17 -0300

    .active open gif

--- scripts/main.js
+++ scripts/main.js
@@ -7,139 +7,193 @@
 		const apiKey 	= 'LanYkWCgNLIRDm6XZOZWnYH9yZHOProA';
 	// 	Parametros
 		let url, limit  = 3, offset = 0;
-		let total, pages;
+		let total, pages, msg = 'favs';
+		let m = 0, s = 0;
 		let phase, nextItem;
 		let likeHit, downHit, openHit;
 /*	Areas de Eventos	*/
-	//	menu Hamburguesa
+	//	Barra de Navegacion
 		const menuBtn	= document.querySelector('#menu');
 		const menuList	= document.querySelector('.menu');
+		const menuItem	= document.querySelectorAll('.menu li');
 	//	Formulario de Busqueda
 		const frmSearch	= document.querySelector('#search')
 		const textField = document.querySelector('#search input')
 		const dataList 	= document.querySelector('#suggestion')
-	//	Resultados
+	//	Area de Resultados
 		const titleArea = document.querySelector('section h1')
 		const gifsArea 	= document.querySelector('#results')
 		const pageArea 	= document.querySelector('#pagination')
 		const trendArea = document.querySelector('#trending');
-	//	Crear Gif
+	//	Seccion de Grabacion
 		const stageArea	= document.querySelectorAll('#crear_gifo .menu li');
 		const gifBtn 	= document.querySelector('#crear_gifo button');
 		const gifMedia 	= document.querySelector('#crear_gifo article video');
 		const gifView	= document.querySelector('#crear_gifo article img');
 		const recAgain	= document.querySelector('#crear_gifo .menu a');
-	//	favoritos
+	//	Seccion de Favoritos
 		const favArea 	= document.querySelector('#favoritos div');
 		const noFavs 	= document.querySelector('#favoritos .noItems')
 /*	Funciones y Metodos	*/
 	//	Consulta API Giphy
-		async function fetchAPI(url, editArea, buildArea) { 
+		async function fetchAPI(url, editArea, buildArea, type = true) { 
 			await fetch(url)
 				.then( resultado => resultado.json()
 				.then( async giphy => { 
 					giphy.pagination ? total = giphy.pagination.total_count : null;
-					giphy.data.forEach( item => editArea.innerHTML += buildArea(item) )
+					giphy.data.forEach( item => editArea.innerHTML += buildArea(item, type) )
 					showPages(url, total);
 					getItems();
+					favLoad();
 		}	)	)	};
 	//	Constructores
-		const showGifs = function(item){ return (
-			`<article id="${item.id}" class="result">
-					<img class="result" 
-						src="${item.images.fixed_height_downsampled.webp}" 
-						alt="${item.title}"
-						title="${item.username}"
-						ismap
-					/>
-					<div class="hidden">
-						<p>
-							${item.username ? item.username : 'anonymous'}
-							<br />
-							<strong>${item.title ? item.title : 'untitled'}</strong>
-						</p>
-						<div class="social">
-							<a class="icon fav"></a>
-							<a class="icon download"></a>
-							<a class="icon max"></a>
-						</div>
-					<div>
-			</article>`
-		)	}
-		const showOptions = function(item) { return (
-			`<option value="${item.name}">
-				${item.name}
-			</option>`
-		)	}
-		const showPages = function(url, total){
-			if (url.includes('search') && total >= 1) { 
-				pageArea.innerHTML =
-					`<ul>
-						<li><strong>Resultados </strong> ${total}</li>
-						<li>
-							<strong>Paginas </strong> 
-							${ ( actual = (offset / 12) + 1 ) <= (pages = Math.ceil(total / 12)) ? actual : pages } 
-							/ ${ pages }
-						</li>
-					</ul>
-					<button class="button">
-						${ offset + limit <= total ? `VER MAS...`: `NO HAY MAS RESULTADOS` }
-					</button>`
-
-			} else if (url.includes('search')){
-				pageArea.innerHTML =
-					`<img src="assets/icons/section_no_results.svg" alt="ouch">
-					<p class="notFound">intenta con otra busqueda</p>`
+		//	Resultados de Busqueda
+			const showGifs = (item, type) => (
+				`<article 
+					id="${type ? item.id : 'fav_'+ item.id}" 
+					class="${ type ? 'result' : 'favorite'}">
+					<img class="${ type ? 'result' : 'favorite'}" 
+						src="${ type ? item.images.fixed_height_downsampled.webp : item.img }" 
+						alt="${ item.title }"
+						title="${ item.username }"
+					ismap />
+					${showActions(item, type)}
+				</article>`
+			);
+			const showActions = (item, type) => (
+				`<div class="hidden">
+					<p>
+						${item.username ? item.username : 'anonymous'}
+						<br />
+						<strong>${item.title ? item.title : 'untitled'}</strong>
+					</p>
+					<div class="social">
+						<a class="icon fav ${ !type ? 'active' : ''}"></a>
+						<a href="${ item.img ? item.img : item.images.fixed_height_downsampled.webp }" class="icon download" target="_blank" download></a>
+						<a class="icon max"></a>
+					</div>
+				</div>`
+			)
+			const showPages = (url, total) => {
+				if (url.includes('search') && total >= 1) { 
+					pageArea.innerHTML =
+						`<ul>
+							<li>
+								<strong>Resultados</strong> 
+								${total}
+								</li>
+							<li>
+								<strong>Paginas</strong> 
+								${ ( actual = (offset / 12) + 1 ) <= (pages = Math.ceil(total / 12)) ? actual : pages } 
+								/ ${ pages }
+							</li>
+						</ul>
+						<button class="button">
+							${ offset + limit <= total ? `VER MAS...`: `NO HAY MAS RESULTADOS` }
+						</button>`
+				} else if (url.includes('search')){
+					noResults(pageArea, 'results');
+				}
 			}
-		}
-		const setPhase = function(type){
-			switch(phase){
-				case 1:
-					startGif();	
-					gifBtn.innerHTML = 'Grabar';
-					gifMedia.classList.add('show'); 
-					gifView.classList.remove('show'); 
+		//	Consejos y Sugerencias
+			const showOptions = (item) => (
+				`<option value="${item.name}">
+					${item.name}
+				</option>`
+			);	
+			const noResults = (editArea, icon) =>{
+				let msg;
+				switch(icon){
+					case 'favs': 
+						msg = `"¡guarda tu primer Gifo en favoritos <br/> para que se muestre aqui!"`; 
 					break;
-				case 2:
-					recGif(); 
-					gifBtn.innerHTML = 'Finalizar';
-					recAgain.innerHTML = `00:00`;
+					case 'results': 
+						msg = `"intenta con otra busqueda"`; 
+					break; 
+					case 'gifs': 
+						msg = `"Animate a crear tu primer GIFO"`; 
 					break;
-				case 3:
-					stopGif(); 
-					gifBtn.innerHTML = 'Subir';
-					gifMedia.classList.toggle('show');
-					gifView.classList.toggle('show');
-					recAgain.innerHTML = `Repetir Captura`
-					break;
-				case 4:
-					question = confirm('¿deseas subir el GIF?')
-						question? uploadGif(): null 		
-						gifBtn.innerHTML = 'Comenzar'
-						gifMedia.classList.toggle('show'); 
-						gifView.classList.toggle('show'); 
-						phase = 1;
-						type = false;
-					break;
+				}
+				editArea.innerHTML = `
+					<img src="assets/icons/section_no_${icon}.svg" alt="ouch">
+					<p class="notFound special">${msg}</p>`
 			}
-			stageArea[phase - 1].classList.add('active')
-			switch (type){
-				case true:
-					phase > 1 ? stageArea[phase - 2].classList.remove('active') : null
-					break;
-				case false:
-					for(i = 0 ; i < stageArea.length; i++){
-						stageArea[i].classList.remove('active');
+		//	Cronometro
+			const timeStart = () => {
+				clock();
+				recTime = setInterval( clock , 999 );
+			}
+			const clock = () => {
+			    var mAux, sAux;
+			    s++;
+			    if(s > 59){ m++; s=0 }
+			    s < 10 ? sAux = "0" + s : sAux=s;
+			    m < 10 ? mAux = "0" + m : mAux=m;
+			    recAgain.innerHTML = `${mAux}:${sAux}`
+			}
+			const timeStop = () => {
+				clearInterval(recTime);
+				m = 0; s = 0;
+			    recAgain.innerHTML = `Repetir Captura`
+			}
+		//	Creacion de Gif	
+			const setPhase = (type) => {
+				//	Etapas de Grabacion
+					switch(phase){
+						case 1:
+							startGif();	
+							gifBtn.innerHTML = 'Grabar';
+							gifMedia.classList.add('show'); 
+							gifView.classList.remove('show'); 
+							break;
+						case 2:
+							recGif(); 
+							gifBtn.innerHTML = 'Finalizar';
+							timeStart();
+							break;
+						case 3:
+							stopGif(); 
+							gifBtn.innerHTML = 'Subir';
+							gifMedia.classList.toggle('show');
+							gifView.classList.toggle('show');
+							timeStop();
+							break;
+						case 4:
+							question = confirm('¿deseas subir el GIF?')
+								question? uploadGif(): null 		
+								gifBtn.innerHTML = 'Comenzar'
+								gifMedia.classList.toggle('show'); 
+								gifView.classList.toggle('show'); 
+								phase = 1;
+								type = false;
+							break;
 					}
-					stageArea[phase - 1].classList.add('active');
-					break;
+				//	Asignacion de Clases
+					stageArea[phase - 1].classList.add('active')
+					switch (type){
+						case true:
+							phase > 1 ? stageArea[phase - 2].classList.remove('active') : null
+							break;
+						case false:
+							for(i = 0 ; i < stageArea.length; i++){
+								stageArea[i].classList.remove('active');
+							}
+							stageArea[phase - 1].classList.add('active');
+							break;
+					}
 			}
-		}
-	//	Menu hamburguesa
-		menuBtn.addEventListener( 'click', () => { 
-			menuList.classList.toggle('open');
-			menuList.classList.contains('open')? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;';
-		}	);
+	//	Barra de Navegacion	
+		//	Menu hamburguesa
+			menuBtn.addEventListener( 'click', () => { 
+				menuList.classList.toggle('open');
+				menuList.classList.contains('open')? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;';
+			}	);
+		//	Item Activo
+			menuItem.forEach( (item, i) => item.addEventListener(
+				'click', () => {
+					for(li = 1 ; li < menuItem.length; li++){
+						li === i ? menuItem[li].classList.toggle('active') : menuItem[li].classList.remove('active');
+			}	}	)	)
 	//	Definicion Cookies
 		var setCookie = function(item){
 			document.cookie = `${item.id} = ${item.images.fixed_height_downsampled.webp}; Max-Age=2600000; Secure`; 
@@ -150,48 +204,32 @@
 			fetchAPI(url, trendArea, showGifs);
 		}	)
 	// 	Elementos Favoritos
-		function favLoad(){
-			if(localStorage.length < 1){
-				noFavs.innerHTML = `
-					<img src="assets/icons/section_no_favs.svg" alt="ouch">
-					<p class="notFound special">
-						"¡guarda tu primer Gifo en favoritos
-						para que se muestre aqui!"
-					</p>`
+		const favLoad = () =>{
+			if( localStorage.length == 0 ){
+				noResults(noFavs, 'favs');
 			} else {
+				favArea.innerHTML = ``;
 				for ( i = 0; i < localStorage.length; i++ ){  
 		  			favGif = localStorage.key(i);
 		  			item = JSON.parse(localStorage.getItem(favGif));
-		 			favArea.innerHTML += `
-		 				<article id="fav_${favGif}">
-						<img class="favorite" 
-							src="${item.img}"
-							title="${item.username}" 
-							alt="${item.title}"
-							ismap />
-						<div class="hidden">
-							<p>
-								${item.username ? item.username : 'anonymous'}
-								<br />
-								<strong>${item.title ? item.title : 'untitled'}</strong>
-							</p>
-							<div class="social">
-								<a class="icon fav active"></a>
-								<a class="icon download"></a>
-								<a class="icon max"></a>
-							</div>
-						<div>
-						</article>`
-		 			document.getElementById(favGif) ? favImg = document.getElementById(favGif).querySelector('img') : favImg = false;
-		 			document.getElementById(favGif) ? favIco = document.getElementById(favGif).querySelector('.fav') : favIco = false;
-		 			if(favImg && favIco){
-		 				favImg.classList.add('active');
-		 				favIco.classList.add('active')
-		 			}
-			}}
-			
+		 			favArea.innerHTML += showGifs(item, false);
+		 			
+				}		
+			} 
 		}
-	//	Elementos Usuario
+		//	Agregar Item
+			const addToFav = () => {
+				window.localStorage.setItem( id , JSON.stringify( {
+					'id' : id,
+					'img': item.src, 
+					'username': item.title, 
+					'title': item.alt
+			}	)	)	} 
+		//	Remover Item
+			const removeFav = () => { 
+				window.localStorage.removeItem( id ) 
+			}
+	//	Elementos del Usuario
 	//	Sugerencias de Busqueda 
 		textField.addEventListener(
 			'input', () => {
@@ -209,6 +247,7 @@
 	//	Resultados Busqueda
 		frmSearch.addEventListener( 'submit', (e) => {
 			e.preventDefault();
+			
 			limit = 12;
 			offset = 0;
 			termino = textField.value;
@@ -220,10 +259,9 @@
 	//	Cargar Paginas
 		pageArea.onsubmit =  (e) => {
 			e.preventDefault();
-			offset = offset + 12;
+			offset += 12;
 			url = `${searchURL}?api_key=${apiKey}&q=${termino}&limit=${limit}&offset=${offset}&rating=g&lang=es`
 			fetchAPI(url, gifsArea, showGifs);
-
 		}
 	//	Creacion de Gifs
 		recAgain.addEventListener( 'click', () => {
@@ -231,10 +269,10 @@
 			setPhase(false);
 		}	)
 		gifBtn.addEventListener( 'click', () => {
-			phase < 4 ? phase = phase + 1: phase = 1;
+			phase < 4 ? phase++ : phase = 1;
 			setPhase(true);
 		}	)
-/*	Funciones asincronas	*/
+/*	Funciones Asincronas	*/
 	// 	Consultar Webcam
 		async function startGif() {
 			const stream = await navigator.mediaDevices.getUserMedia({
@@ -244,7 +282,7 @@
 			gifMedia.srcObject = stream;
 			await gifMedia.play();
 		}
-	//	Comenza Grabacion
+	//	Comenzar Grabacion
 		async function recGif() {
 			const stream = gifMedia.srcObject;
 			videoRecorder = new RecordRTCPromisesHandler(stream, {
@@ -323,43 +361,51 @@
 		};
 /* 	Acciones del Usuario */
 	//	Elementos Utilizables
-		var getItems = function(){
-			likeHit = document.querySelectorAll('.result + .hidden .fav');
-			downHit = document.querySelectorAll('.result + .hidden .download');
-			openHit = document.querySelectorAll('.result + .hidden .max');
-			userAction();
+		const getItems = () => {
+			//	Resultados
+			likeHit	= document.querySelectorAll('.result .fav');
+			downHit = document.querySelectorAll('.result .download');
+			openHit = document.querySelectorAll('.result .max');
+			//	Favoritos
+			favHit 	= document.querySelectorAll('.favorite .fav');
+			favOpen	= document.querySelectorAll('.favorite .max')
+			//	Mis Gifos
+
+			userActions();
 		}
 	//	Botones de Accion
-		var userAction = function (){
+		const userActions = () => {
 		//	Like Button
-			likeHit.forEach( (like , i) => like.addEventListener( "click", () => { 
-				artKey = document.querySelectorAll('article.result')[i];
-				item = document.querySelectorAll('img.result')[i];
-				artKey.id.includes('fav_') ? key = artKey.id.slice(4) : key = artKey.id;
+			likeButtons();
+			openButtons();
+		//	Download Button
+			// downHit.forEach( (down , i) => down.addEventListener( "click", () => {
+			// 	item = document.querySelectorAll('img.result')[i].src
+			// 	a = document.createElement('a');
+			// 	a.download = true;
+			// 	a.target = '_blank';
+			// 	a.href= item;
+			// 	a.click();
+			// }	)	)
+		//	Open Button
+		}
+		const likeButtons = () => {
+			likeHit.forEach( ( like , i ) => like.addEventListener( 'click', () => { 
+				totalItems('result', i);
 				like.classList.toggle('active');
-				like.classList.contains('active') ? 	
-					window.localStorage.setItem( key , JSON.stringify( {
-						'img': item.src, 
-						'username': item.title, 
-						'title': item.alt
-					}	)	) : ( window.localStorage.removeItem( key )
-					)
+				like.classList.contains('active') ? addToFav() : removeFav();
 				favArea.innerHTML = ``;
 				noFavs.innerHTML = ``;
-				favLoad()
-				}	)	);
-		//	Download Button
-			downHit.forEach( (down , i) => down.addEventListener( "click", () => {
-				item = document.querySelectorAll('img.result')[i].src
-				a = document.createElement('a');
-				a.download = true;
-				a.target = '_blank';
-				a.href= item;
-				a.click();
-			}	)	)
-		//	Open Button
-			openHit.forEach( open => open.addEventListener("click", () => {	
-				alert('vas a maximizar')
-			}	)	)	
-			favLoad();
+				favLoad();
+		}	)	)	}
+		const openButtons = () => {
+			openHit.forEach( ( open, i ) => open.addEventListener('click', () => {	
+				totalItems('result', i);
+				open.classList.toggle('max');
+				open.classList.toggle('close')
+				document.getElementById(id).classList.toggle('active');
+		}	)	)	}
+		const totalItems = (param, i) => { 
+			id = document.querySelectorAll('article.'+param)[i].id;
+			item = document.querySelectorAll('img.'+param)[i];
 		}
